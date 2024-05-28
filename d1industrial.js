7ad2f7c79036c691ec79fd058dc3d105b37e2936
Jerry OG <42327533+jeriprojects@users.noreply.github.com>
2024-05-28 01:55:04 +0100

    Create d1industrial.js

---
+++ d1industrial.js
@@ -0,0 +1,24 @@
+ $(document).ready(function(){
+ $('#nav3').append('<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="nav-btn navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a href="#" class="brand"><img src="img/logo-dark.png" alt="Factorial" /></a>
+					</div>
+					<div id="navbar" class="collapse navbar-collapse navbar-right">
+						<ul class="nav navbar-nav nav-menu">
+                            <li class="active"><a data-scroll href="#home">Home <span class="sr-only">(current)</span></a></li>
+							<li><a data-scroll href="#services">Service</a></li>
+							<li><a data-scroll href="#works">Works</a></li>
+							<li><a data-scroll href="#testimonial">Reviews</a></li>
+							<li><a data-scroll href="#blog">News</a></li>
+							<li><a data-scroll href="#contact">Contact</a></li>
+                        </ul>
+                        <a href="#" class="nav_btn">Build Now</a>
+					</div><!--/.navbar-collapse -->
+				</div>');			
+		});
